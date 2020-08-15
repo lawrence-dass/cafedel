e5ce0998d971d0b3d7952be06e49fde457ab04e3
Lawrence <Lawrence4code@gmail.com>
2020-08-15 22:18:28 +0530

    feat: setup postgres connection with local db and create all require db queries

--- server/server.js
+++ server/server.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 
+const db = require('./db');
+
+
+
 // express instance 
 const app = express();
 
@@ -11,53 +15,82 @@ app.use(morgan("dev"));
 app.use(express.json())
 
 // get all restautants
-app.get('/restaurants', (req, res) => {
-  res.status(200).json({
-    status: "success",
-    data: {
-      restaurants: [" barista", "cafe coffee day"]
-    }
-  })
+app.get('/restaurants', async (req, res) => {
+  try {
+    const results = await db.query('SELECT * FROM restaurants')
+    res.status(200).json({
+      status: "success",
+      results: results.rows.length,
+      data: {
+        restaurants: results.rows
+      }
+    })
+  } catch (err) {
+    console.log({ err });
+  }
+
 })
 
 // get a single restaurant
-app.get('/restaurants/:id', (req, res) => {
-  res.status(200).json({
-    status: "success",
-    data: {
-      "restaurant": "dummy single"
-    }
-  })
+app.get('/restaurants/:id', async (req, res) => {
+  try {
+    // parameterized query // avoid string interpolation
+    const results = await db.query('SELECT * FROM restaurants WHERE id = $1', [req.params.id]);
+    res.status(200).json({
+      status: "success",
+      data: {
+        "restaurant": results.rows[0]
+      }
+    })
+  } catch (err) {
+    console.log({ err })
+  }
 })
 
 // create a restaurant
-app.post('/restaurants', (req, res) => {
-  res.status(201).json({
-    status: "success",
-    data: {
-      "restaurant": "dummy created"
-    }
-  })
+app.post('/restaurants', async (req, res) => {
+  try {
+    const results = await db.query('INSERT INTO restaurants (name, location, price_range ) VALUES ($1, $2, $3) returning *', [req.body.name, req.body.location, req.body.price_range])
+    res.status(201).json({
+      status: "success",
+      data: {
+        "restaurant": results.rows[0]
+      }
+    })
+  } catch (err) {
+    console.log({ err });
+  }
+
 })
 
 // update restaurant
-app.put('/restaurants/:id', (req, res) => {
-  res.status(200).json({
-    status: "success",
-    data: {
-      "restaurant": "dummy updated"
-    }
-  })
+app.put('/restaurants/:id', async (req, res) => {
+  try {
+    const results = await db.query('UPDATE restaurants SET name = $1, location = $2, price_range = $3 WHERE id = $4 returning *', [req.body.name, req.body.location, req.body.price_range, req.params.id])
+    res.status(200).json({
+      status: "success",
+      data: {
+        "restaurant": results.rows[0]
+      }
+    })
+  } catch (err) {
+    console.log({ err });
+  }
 })
 
 // delete restaurant
-app.delete('/restaurants/:id', (req, res) => {
-  res.status(200).json({
-    status: "success",
-    data: {
-      "restaurant": "dummy deleted"
-    }
-  })
+app.delete('/restaurants/:id', async (req, res) => {
+  try {
+    const results = await db.query('DELETE FROM restaurants WHERE id = $1', [req.params.id])
+    res.status(200).json({
+      status: "success",
+      data: {
+        "restaurant": "dummy deleted"
+      }
+    })
+  } catch (err) {
+    console.log({ err })
+  }
 })
 
 
