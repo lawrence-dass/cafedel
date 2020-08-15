6fb7559c37531a44908d046c4c6126aeba59a100
Lawrence <Lawrence4code@gmail.com>
2020-08-15 15:44:30 +0530

    add dotevn for environment variables and update gitignore file

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
+require('dotenv').config();
 const express = require('express');
+
 
 const app = express();
 
 
 
-const port = 8000
+
+const port = process.env.PORT || 7000
 
 app.listen(port, () => {
   console.log(`Server started at port ${port}`)
