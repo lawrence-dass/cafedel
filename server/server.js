2cc626c7cb4b48ab0df681c044d63e83982bae7b
Lawrence <Lawrence4code@gmail.com>
2020-08-15 18:46:33 +0530

    add basic crud route with dummy data

--- server/server.js
+++ server/server.js
@@ -1,13 +1,67 @@
 require('dotenv').config();
 const express = require('express');
+const morgan = require('morgan');
 
-
+// express instance 
 const app = express();
 
 
+// middlewares
+app.use(morgan("dev"));
+app.use(express.json())
+
+// get all restautants
+app.get('/restaurants', (req, res) => {
+  res.status(200).json({
+    status: "success",
+    data: {
+      restaurants: [" barista", "cafe coffee day"]
+    }
+  })
+})
+
+// get a single restaurant
+app.get('/restaurants/:id', (req, res) => {
+  res.status(200).json({
+    status: "success",
+    data: {
+      "restaurant": "dummy single"
+    }
+  })
+})
+
+// create a restaurant
+app.post('/restaurants', (req, res) => {
+  res.status(201).json({
+    status: "success",
+    data: {
+      "restaurant": "dummy created"
+    }
+  })
+})
+
+// update restaurant
+app.put('/restaurants/:id', (req, res) => {
+  res.status(200).json({
+    status: "success",
+    data: {
+      "restaurant": "dummy updated"
+    }
+  })
+})
+
+// delete restaurant
+app.delete('/restaurants/:id', (req, res) => {
+  res.status(200).json({
+    status: "success",
+    data: {
+      "restaurant": "dummy deleted"
+    }
+  })
+})
 
 
-const port = process.env.PORT || 7000
+const port = process.env.PORT || 7000;
 
 app.listen(port, () => {
   console.log(`Server started at port ${port}`)
